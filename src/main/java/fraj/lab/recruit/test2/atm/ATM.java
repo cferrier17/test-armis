47050799b90c945130016557f439d6d1748dd813
Corentin Ferrier <ferrier.corentin@wanadoo.fr>
2023-08-24 15:25:14 +0200

    Add ATM class testing & fix it

--- src/main/java/fraj/lab/recruit/test2/atm/ATM.java
+++ src/main/java/fraj/lab/recruit/test2/atm/ATM.java
@@ -5,41 +5,43 @@ package fraj.lab.recruit.test2.atm;
  */
 public class ATM {
 
-	private final AmountSelector amountSelector;
-	private final CashManager cashManager;
-	private final PaymentProcessor paymentProcessor;
+    private final AmountSelector amountSelector;
+    private final CashManager cashManager;
+    private final PaymentProcessor paymentProcessor;
 
-	/**
-	 * Constructs an {@link ATM} with a set of associated components.
-	 * 
-	 * @param argAmountSelector   the {@link AmountSelector} used to have the user
-	 *                            select the wanted amount
-	 * @param argCashManager      the {@link CashManager} that deals with bank notes
-	 * @param argPaymentProcessor the {@link PaymentProcessor} that deals with
-	 *                            payment operations
-	 */
-	public ATM(AmountSelector argAmountSelector, CashManager argCashManager, PaymentProcessor argPaymentProcessor) {
+    /**
+     * Constructs an {@link ATM} with a set of associated components.
+     *
+     * @param argAmountSelector   the {@link AmountSelector} used to have the user
+     *                            select the wanted amount
+     * @param argCashManager      the {@link CashManager} that deals with bank notes
+     * @param argPaymentProcessor the {@link PaymentProcessor} that deals with
+     *                            payment operations
+     */
+    public ATM(AmountSelector argAmountSelector, CashManager argCashManager, PaymentProcessor argPaymentProcessor) {
 
-		amountSelector = argAmountSelector;
-		cashManager = argCashManager;
-		paymentProcessor = argPaymentProcessor;
+        amountSelector = argAmountSelector;
+        cashManager = argCashManager;
+        paymentProcessor = argPaymentProcessor;
 
-	}
+    }
 
-	/**
-	 * Runs a cash withdrawal session on the ATM.
-	 */
-	public ATMStatus runCashWithdrawal() throws ATMTechnicalException {
-		int locAmount = amountSelector.selectAmount();
-		if (locAmount <= 0) {
-			throw new ATMTechnicalException();
-		}
-		if (! cashManager.canDeliver(locAmount)) {
-			return ATMStatus.CASH_NOT_AVAILABLE;
-		}
-		paymentProcessor.pay(locAmount);
-		cashManager.deliver(locAmount);
-		return ATMStatus.DONE;
-	}
+    /**
+     * Runs a cash withdrawal session on the ATM.
+     */
+    public ATMStatus runCashWithdrawal() throws ATMTechnicalException {
+        int locAmount = amountSelector.selectAmount();
+        if (locAmount <= 0) {
+            throw new ATMTechnicalException();
+        }
+        if (!cashManager.canDeliver(locAmount)) {
+            return ATMStatus.CASH_NOT_AVAILABLE;
+        }
+        if (PaymentStatus.FAILURE.equals(paymentProcessor.pay(locAmount))) {
+            return ATMStatus.PAYMENT_REJECTED;
+        }
+        cashManager.deliver(locAmount);
+        return ATMStatus.DONE;
+    }
 
 }
